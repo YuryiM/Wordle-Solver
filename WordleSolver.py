9e37081df7ba5050abc78cb52047bc3c13a350e3
YM <ymironchyk@gmail.com>
2022-09-16 09:44:41 -0700

    Added comments, removed print statements

--- WordleSolver.py
+++ WordleSolver.py
@@ -1,12 +1,17 @@
+# Read file and create list of words based on file
 words = open("valid-wordle-words.txt", "rt").readlines()
+# Create 2d array where each word is its own array, remove newline char
 words = [list(word.rstrip()) for word in words]
 
 print("Enter letters without spaces")
 print("Ex. To enter 'A' and 'G', enter 'AG'")
 
+# Letters which are not in the final word
 bad = set(input("Enter bad letters: ").strip().lower())
+# Letters which are in the final word, but in an unknown position
 good = set(input("Enter good letters: ").strip().lower())
 
+# Remove words based on above requirements 
 i = 0
 # Iterate through every word
 while i < len(words):
@@ -20,11 +25,11 @@ while i < len(words):
         i -= 1
     i += 1
 
-print(["".join(word) for word in words])
 print("Enter the known letters in the proper positions")
 print("with unknown letters represented as an underscore (case insensitive)")
 print("Ex. R_A_G")
 
+# Letters in known position and unknown letters
 placed = list(input("Enter the placed letters: ").strip().lower())
 
 i = 0
@@ -40,12 +45,13 @@ while i < len(words):
             break
         j += 1
     i += 1  
+
 # Prints result
 if len(words) > 1:
     print("Here are the possible solutions:")
     print(["".join(word) for word in words])
 elif len(words) == 1:
-    print("Here is your solution")
-    print(words[0])
+    print("Here is your solution:")
+    print("".join(words[0]))
 else:
     print("No words match your input, please try again")
